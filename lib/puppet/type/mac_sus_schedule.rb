d1f134617d0be90b4ec51ae5b0ec0ecc4ac6829c
Graham Gilbert <graham@grahamgilbert.com>
2013-12-02 18:12:50 +0000

    Can use ensure => 'on' and ensure => 'off' now

--- lib/puppet/type/mac_sus_schedule.rb
+++ lib/puppet/type/mac_sus_schedule.rb
@@ -1,8 +1,25 @@
 Puppet::Type.newtype(:mac_sus_schedule) do
   @doc = "Enables and disables automatic software update checks"
   
-  ensurable
-  
   newparam(:name, :namevar => true)
+  
+  ensurable do
+          desc "Possible values are *present*, *absent*, *on*, and *off*."
+
+          newvalue(:absent) do
+              if @resource.provider.exists?
+                  @resource.provider.destroy
+              end 
+          end
+          aliasvalue(:off, :absent)
+
+          newvalue(:present) do
+              unless @resource.provider.exists?
+                  @resource.provider.create
+              end 
+          end 
+          aliasvalue(:on, :present)
+
+      end 
   
 end
