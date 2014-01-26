462f50d5eb4dfff6932ade158c131ebe14cb758e
Graham Gilbert <graham@grahamgilbert.com>
2014-01-26 09:39:19 +0000

    Show full username and password boxes on the login screen

---
+++ manifests/login_window_showfullname.pp
@@ -0,0 +1,17 @@
+# == Class: mac_admin::login_window_hostname
+#
+# Installs a configuration profile to show the hostname at a Mac's login window
+#
+# === Example
+#
+#  class { 'mac_admin::lowing_window_hostname': }
+#
+
+class mac_admin::login_window_showfullname(
+) inherits mac_admin::params {
+    ##Install the profile
+    mac_profiles_handler::manage { 'com.grahamgilbert.login_window_showfullname':
+        ensure      => present,
+        file_source => 'puppet:///modules/mac_admin/login_window_showfullname/com.grahamgilbert.login_window_showfullname.mobileconfig',
+    }
+}
