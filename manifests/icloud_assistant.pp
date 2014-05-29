cf64e5539daa89937e09d8fe82d8a920d9842fab
Graham Gilbert <graham@grahamgilbert.com>
2014-05-29 06:48:06 +0100

    The profile isn't working on 10.9 for iCloud, use defaults in the user template instead.

--- manifests/icloud_assistant.pp
+++ manifests/icloud_assistant.pp
@@ -14,4 +14,21 @@ class mac_admin::icloud_assistant(
         ensure      => present,
         file_source => 'puppet:///modules/mac_admin/icloud_assistant/com.grahamgilbert.icloud_assistant.mobileconfig',
     }
+
+    # Defaults as the profile isn't working
+
+    mac_admin::osx_defaults { 'DidSeeCloudSetup':
+        ensure => present,
+        domain => '/System/Library/User\ Template/Non_localized/Library/Preferences/com.apple.SetupAssistant',
+        key    => 'DidSeeCloudSetup',
+        value  => true,
+        type   => 'bool',
+    }
+
+    mac_admin::osx_defaults { 'LastSeenCloudProductVersion':
+        ensure => present,
+        domain => '/System/Library/User\ Template/Non_localized/Library/Preferences/com.apple.SetupAssistant',
+        key    => 'LastSeenCloudProductVersion',
+        value  => '10.9.3',
+    }
 }
