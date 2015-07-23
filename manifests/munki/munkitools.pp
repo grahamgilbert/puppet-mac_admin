453d02f5b9a231cd894cf1562a1c469365c3b4ce
Graham Gilbert <graham@grahamgilbert.com>
2015-07-23 12:02:21 +0100

    Check the installed version before installing

--- manifests/munki/munkitools.pp
+++ manifests/munki/munkitools.pp
@@ -1,7 +1,10 @@
 class mac_admin::munki::munkitools {
-    package { 'munki_tools2':
-        ensure   => installed,
-        provider => pkgdmg,
-        source   => 'https://github.com/munki/munki/releases/download/v2.2.4/munkitools-2.2.4.2431.pkg',
+
+    if $::mac_munki_version < "2.2.4.2431" {
+        package { 'munki_tools2':
+            ensure   => installed,
+            provider => pkgdmg,
+            source   => 'https://github.com/munki/munki/releases/download/v2.2.4/munkitools-2.2.4.2431.pkg',
+        }
     }
 }
