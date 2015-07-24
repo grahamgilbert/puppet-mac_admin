f9b2dc0e0708a7ecac2c0beba19e4d8b1c684715
Graham Gilbert <graham@grahamgilbert.com>
2015-07-24 18:32:16 +0100

    Disable airdrop class

---
+++ manifests/disable_airdrop.pp
@@ -0,0 +1,17 @@
+# == Class: mac_admin::disable_airdrop
+#
+# Installs a configuration profile to disable AirDrop
+#
+# === Example
+#
+#  class { 'mac_admin::disable_airdrop': }
+#
+
+class mac_admin::disable_airdrop(
+) inherits mac_admin::params {
+    ##Install the profile
+    mac_profiles_handler::manage { 'com.grahamgilbert.disable_airdrop':
+        ensure      => present,
+        file_source => 'puppet:///modules/mac_admin/disable_airdrop/com.grahamgilbert.disable_airdrop.mobileconfig',
+    }
+}
