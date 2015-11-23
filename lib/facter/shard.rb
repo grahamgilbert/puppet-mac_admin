8d35b5de6e526c3a5a80a8da582f3dc0d929a36f
Graham Gilbert <graham@grahamgilbert.com>
2015-11-23 19:33:14 +0000

    Look for files to stop being part of sharding

--- lib/facter/shard.rb
+++ lib/facter/shard.rb
@@ -5,11 +5,17 @@ require 'digest'
 Facter.add('shard') do
 confine :kernel => "Darwin"
     setcode do
-        MOD_VALUE = 10000
-        serial = Facter.value(:sp_serial_number)
-        digest = Digest::SHA256.hexdigest serial
-        sha256 = digest.to_i(16)
-        shard = ((sha256 % MOD_VALUE) * 100) / MOD_VALUE.to_f
-        shard.to_i
+        if File.exist?('/usr/local/shard/production')
+            100
+        elsif File.exist?('/usr/local/shard/testing')
+            1
+        else                
+            MOD_VALUE = 10000
+            serial = Facter.value(:sp_serial_number)
+            digest = Digest::SHA256.hexdigest serial
+            sha256 = digest.to_i(16)
+            shard = ((sha256 % MOD_VALUE) * 100) / MOD_VALUE.to_f
+            shard.to_i
+        end
     end
 end
