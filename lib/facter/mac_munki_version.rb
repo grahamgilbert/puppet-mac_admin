002f9d9d6384471047db9cb2e8c6690134d27b8e
Graham Gilbert <graham@grahamgilbert.com>
2015-11-30 18:21:32 +0000

    Output something is munki not installed

--- lib/facter/mac_munki_version.rb
+++ lib/facter/mac_munki_version.rb
@@ -2,6 +2,10 @@
 Facter.add(:mac_munki_version) do
   confine :kernel => "Darwin"
   setcode do
-    Facter::Util::Resolution.exec("/usr/local/munki/managedsoftwareupdate --version")
+    if File.exist?('/usr/local/munki/managedsoftwareupdate')
+        Facter::Util::Resolution.exec("/usr/local/munki/managedsoftwareupdate --version")
+    else
+        "Munki not installed"
+    end
   end
 end
