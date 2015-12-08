78136f739a0fe9a6c92479d80bacae9d079fdf62
Graham Gilbert <graham@grahamgilbert.com>
2015-12-08 16:53:19 +0000

    Quote strings for puppet 4

--- manifests/sus.pp
+++ manifests/sus.pp
@@ -33,29 +33,29 @@ class mac_admin::sus(
         default:{
             fail('Unsupported version of OS X.')
         }
-        10.7:{
+        '10.7':{
             if $sus_url_107 != false {
                 $sus_url = $sus_url_107
             }
         }
-        10.8:{
+        '10.8':{
             if $sus_url_108 != false{
                 $sus_url = $sus_url_108
             }
         }
-        10.9:{
+        '10.9':{
             if $sus_url_109!=false{
                 $sus_url = $sus_url_109
             }
         }
 
-        10.10:{
+        '10.10':{
             if $sus_url_1010!=false{
                 $sus_url = $sus_url_1010
             }
         }
 
-        10.11:{
+        '10.11':{
             if $sus_url_1011!=false{
                 $sus_url = $sus_url_1011
             }
